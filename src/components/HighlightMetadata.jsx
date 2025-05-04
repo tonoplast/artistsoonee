6636b67d30fa7cecacf22129817a5dfb1ca11c72
Sung Wook Chung <tonoplast.chloroplast@gmail.com>
2025-05-04 21:46:36 +1000

    Update HighlightMetadata.jsx

--- src/components/HighlightMetadata.jsx
+++ src/components/HighlightMetadata.jsx
@@ -1,13 +1,13 @@
 const highlightMetadata = {
     
-    "00379_20250406.jpg": { title: "Her - Hanji Series", size: "18 x 26 cm" },
-    "00365_20250406.jpg": { title: "Her - Hanji Series", size: "20 x 20 cm" },
+    "00379_20250406.jpg": { title: "Her - Hanji Series (2024)", size: "18 x 26 cm" },
+    "00365_20250406.jpg": { title: "Her - Hanji Series (2024)", size: "20 x 20 cm" },
     "00351_20240626.jpg": { title: "Her - Fragments of Identity (2024)", size: "73 x 91 cm" },
     "00349_20240503.jpg": { title: "Her - Ink Reverie (2024)", size: "73 x 91 cm" },
     "00202_20190413.jpg": { title: "Tribute - Monroe (2019)", size: "55 x 38 cm" },
     "00184_20180710.jpg": { title: "The Masquerade (2018)", size: "61 x 73 cm" },
-    "00132_20140413.jpg": { title: "Peace - Autumn (2014)", size: "50 x 50 cm" },
-    "00129_20140413.jpg": { title: "Peace - Summer (2014)", size: "50 x 50 cm" },
+    "00132_20140413.jpg": { title: "Peace - Autumn (2013)", size: "50 x 50 cm" },
+    "00129_20140413.jpg": { title: "Peace - Summer (2013)", size: "50 x 50 cm" },
     "00036_20110413.jpg": { title: "Blessing (2011)", size: "65 x 80 cm" },
     "00024_20080516.jpg": { title: "Adversity (2008)", size: "100 x 80 cm" },
     "00021_20080413.jpg": { title: "Vortex (2008)", size: "91 x 117 cm" },
