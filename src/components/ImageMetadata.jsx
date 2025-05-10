339613ab6b80ac367a30a5d2b44e036f807ff51a
Sung Wook Chung <tonoplast.chloroplast@gmail.com>
2025-05-10 10:59:00 +1000

    delete some paintings

--- src/components/ImageMetadata.jsx
+++ src/components/ImageMetadata.jsx
@@ -1,8 +1,18 @@
 const imageMetadata = {
-    // "00368_20250406.jpg": { title: "test", size: "24 x 36 inches" },
-    "00368_2025040z.jpg": { title: "test", size: "24 x 36 inches" },
-    "00002_20230420.jpg": { title: "Serenity", size: "30 x 40 inches" },
-    // ... other mappings
+    "00375_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "46 x 53 cm" },
+    "00374_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "46 x 53 cm" },
+    "00373_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "46 x 53 cm" },
+    "00372_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "46 x 53 cm" },
+    "00371_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "46 x 53 cm" },
+    "00370_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "91 x 117 cm" },
+    "00369_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "38 x 46 cm" },
+    "00368_20250406z.jpg": { title: "Her - Hanji Series (2024)", size: "20 x 20 cm" },
+    "00367_20250406z.jpg": { title: "Her - Hanji Series (2024)", size: "20 x 20 cm" },
+    "00366_20250406z.jpg": { title: "Her - Hanji Series (2024)", size: "20 x 20 cm" },
+    "00365_20250406z.jpg": { title: "Her - Hanji Series (2024)", size: "20 x 20 cm" },
+    "00364_20250406z.jpg": { title: "Her - Hanji Series (2024)", size: "20 x 20 cm" },
+    "00363_20250406z.jpg": { title: "Her - Hanji Series (2025)", size: "73 x 91 cm" },
+
 };
 
 export default imageMetadata;
